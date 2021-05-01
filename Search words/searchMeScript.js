0c2a7257f3ed5bbf2fd0b883fcd7484c47923246
Devadharshini-K <83006930+Devadharshini-K@users.noreply.github.com>
2021-05-01 10:05:33 +0530

    Delete Search words directory

--- Search words/searchMeScript.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    document
-      .getElementById("search-word")
-      .addEventListener("mouseover", function () {
-        $("#search-word").addClass("bg-hovered");
-        console.log(this);
-      });
-  };
