36a412dd17b8d89f693938ad7865173dcb39c6b6
Devadharshini-K <83006930+Devadharshini-K@users.noreply.github.com>
2021-05-01 10:05:47 +0530

    Delete todoList directory

--- todoList/todoListScript.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#sub").click(function(){
-        if($("#task").val()==''){
-            alert("Enter task details!!");
-        }
-        $("#list").append('<li>'+$("#task").val()+'</li>');
-    })
-})
